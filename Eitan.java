0aa2828e088f7a4e7b23f9a7c36433ab30d686eb
eitan <eitan9994@gmail.com>
2024-01-26 11:36:41 +0200

    changes to Eitan

--- Eitan.java
+++ Eitan.java
@@ -1,15 +1,8 @@
-import java.util.concurrent.TimeUnit;
-
 public class Eitan{
 	public static void main(String[] args){
 		System.out.println("I'm blue/n");
 		for(int i=1;i<=7;i++){
 			System.out.println("if i go greed i will die");
-			try{
-			TimeUnit.SECONDS.sleep(1);
-			}
-			catch (InterruptedException e){
-			}
 		}
 	}
 }
